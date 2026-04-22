8962d385cbd185a62523d4041a68e720c1c8627d
Tiago Souza <tiago0214@gmail.com>
2026-04-21 22:35:59 -0300

    feat: add primevue library

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Button from 'primevue/button'
 </script>
 
 <template>
   <HelloWorld />
+  <Button label="Click Me"/>
 </template>
