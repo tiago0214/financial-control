b2c118684eeffb2514496cc99759ac8a1eebcac2
Tiago Souza <tiago0214@gmail.com>
2026-05-08 18:42:29 -0300

    fix: adjust base path

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/pmv-si-2026-1-pe3-t3-grupo-04/',
   server: {
     host: true, // Listen on all local IPs
     port: 5173,
