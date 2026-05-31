00008fe95bb3fed50dd4ed6fe48a1c3b2718d423
Tiago Souza <tiago0214@gmail.com>
2026-05-31 15:56:38 -0300

    refactor: update requirements and restructure project files
    - Removed unused dependencies from requirements.txt
    - Updated uvicorn port in run.py
    - Added new __init__.py files for Models and Settings directories
    - Created metadata.py for SQLAlchemy metadata management

--- src/backend/run.py
+++ src/backend/run.py
@@ -1,9 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "src.main.server.server:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+    uvicorn.run("src.main.server.server:app", host="0.0.0.0", port=3001, reload=True)
