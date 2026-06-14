c8a33458508d44a526ba772e57cc01d08ba90eb2
Tiago Souza <tiago0214@gmail.com>
2026-06-14 01:29:46 -0300

    feat: start the UserRepository implementation

--- src/backend/src/models/repository/users_repository.py
+++ src/backend/src/models/repository/users_repository.py
@@ -1,4 +1,25 @@
+from sqlalchemy import insert, select
 from src.models.entities.users import Users
 from src.models.settings.database_connection_handler import DBConnectionHandler
 
-#class UsersRepository:
+class UsersRepository:
+    async def insert_user(self, user_infos: dict) -> None:
+        async with DBConnectionHandler() as db:
+            query = insert(Users).values(**user_infos)
+
+            await db.session.execute(query)
+            await db.session.commit()
+
+    async def get_user_by_name(self, user_name):
+        async with DBConnectionHandler() as db:
+            query = (
+                select(Users)
+                .where(Users.c.user_name == user_name)
+            )
+
+            result = await db.session.execute(query)
+            rows = result.fetchall()
+
+            users_list = [dict(row._mapping) for row in rows]
+
+            return users_list
---
+++ src/backend/src/models/repository/users_repository_test.py
@@ -0,0 +1,23 @@
+import pytest
+from .users_repository import UsersRepository
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason="Insert in the db")
+async def test_insert_user():
+    user = {
+        "user_name": "TestUser",
+        "email": "test@test.com",
+        "password_hash": "senha_segura"
+    }
+
+    repo = UsersRepository()
+
+    await repo.insert_user(user)
+
+@pytest.mark.asyncio
+async def test_get_user_by_name():
+    repo = UsersRepository()
+
+    result = await repo.get_user_by_name("TestUser")
+
+    print(result)
